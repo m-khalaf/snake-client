9f1e2195e6c198f165c61f31392dabfb33af16fc
m-khalaf <mkhalaf2211@gmail.com>
2022-08-09 21:39:35 +0000

    Create new input file and move the functions handing user input to this file

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const connect = require('./client');
-
-const handleUserInput = function (input) {
-  if (input === '\u0003') {
-    process.exit();
-  };
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-}
+const setupInput = require("./input");
 
 
 console.log("Conecting ...");
